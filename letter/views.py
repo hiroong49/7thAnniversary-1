1241108420b31b8cde06f25efef7cf0d3d6a7e21
sa46lll <9812smh@naver.com>
2022-04-15 19:42:29 +0900

    feat: afterhome post 배치, url 연결

--- letter/views.py
+++ letter/views.py
@@ -9,7 +9,8 @@ def home(requests):
 
 
 def afterhome(requests):
-    return render(requests, 'after_home.html')
+    posts = Post.objects.all()
+    return render(requests, 'after_home.html', {'posts': posts})
 
 
 def icon(requests):
@@ -17,18 +18,18 @@ def icon(requests):
 
 
 def writing(requests):
-    posts = Post.objects # Post.objects를 posts 변수에 담기
+    posts = Post.objects  # Post.objects를 posts 변수에 담기
 
     # POST vs GET 분기
     if requests.method == 'POST':
         form = PostForm(requests.POST, requests.FILES)   # form 변수에 PostForm 할당
         if form.is_valid():     # form 유효성 검증
             form.save()         # 내용 저장
-            return redirect('home') # home 페이지로 가기
+            return redirect('home')  # home 페이지로 가기
     else:
         form = PostForm()   # 빈 form 열기
         
-    return render(requests, 'letter_writing.html', {'posts':posts, 'form':form}) # 인자 3개 : request, template, context
+    return render(requests, 'letter_writing.html', {'posts': posts, 'form': form})  # 인자 3개 : request, template, context
 
 
 def do_duplicate_check(request):
@@ -49,5 +50,5 @@ def do_duplicate_check(request):
 
 def afterletter(requests, pk):  # request와 pk도 인자로 받음
     post = get_object_or_404(Post, pk=pk)       # 해당 객체가 있으면 가져오고 없으면 404에러, pk로 pk 사용
-    return render(requests, 'after_letter.html', {'post':post})
+    return render(requests, 'after_letter.html', {'post': post})
 
