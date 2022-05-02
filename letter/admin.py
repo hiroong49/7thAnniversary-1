2eff54ffd75e14c623990551014fb8b8b3c71b5d
sa46lll <9812smh@naver.com>
2022-05-02 14:45:51 +0900

    feat: admin 페이지 수정

--- letter/admin.py
+++ letter/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Post
 
-# Register your models here.
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'letter_paper', 'photo_tag']
+
+    def photo_tag(self, obj):
+        return format_html('<img src="{}" width="50px;"/>'.format(obj.photo.url))
+    photo_tag.short_description = '사진'
+
+
+admin.site.register(Post, PostAdmin)
