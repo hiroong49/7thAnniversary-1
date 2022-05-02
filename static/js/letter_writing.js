0b1c93392fa6e405e09f5861dbd73981ce33b7a0
sa46lll <9812smh@naver.com>
2022-05-03 03:53:46 +0900

    feat: 닉네임 중복 확인, 예외 처리

--- static/js/letter_writing.js
+++ static/js/letter_writing.js
@@ -10,27 +10,47 @@ function readFile(event){
        reader.readAsDataURL(event.target.files[0]);
 }
 
-
 function duplicate_check(){
-    alert('클릭 이벤트 발생');
-    const author = document.getElementById("uid").value;
-    console.log(author);
+    // 닉네임을 변경했을 경우
+    $('.author_input').change(function () {
+            $('#id_check_success').hide();
+            $('.duplicate_button').show();
+            $('.author_input').attr("check_result", "fail");
+    })
 
-    // do_duplicate_check 함수 연결
-    /*
+    // do_duplicate_check 함수에 json 전달
+    let author_input = document.getElementById("uid");
     $.ajax({
-        url: "{% url 'letter_writing:do_duplicate_check' %}",
-        data: {'author': author},
+        url: "/do_duplicate_check",
+        data: {'author': author_input.value},
         datatype: 'json',
 
         success: function (data) {
             if (data['duplicate'] == "fail") {
-            return false;
-          } else {
-            return ;
-          }
+                alert("이미 존재하는 닉네임 입니다.");
+                author_input.focus();
+                return;
+            } else {
+                alert("사용 가능한 닉네임 입니다.");
+                $('.author_input').attr("check_result", "success");
+                $('#id_check_success').show();
+                $('.duplicate_button').hide();
+                return;
+            }
         }
-      });
-     */
+    });
+}
 
+function submit_check() {
+    // 닉네임 중복 체크
+    if ($('.author_input').attr("check_result") == "fail"){
+        alert("닉네임 중복체크를 해주시기 바랍니다.");
+        $('.username_input').focus();
+        return false;
+    }
+    // 폼 submit
+    else if ($('.author_input').attr("check_result") == "success") {
+        document.getElementById('frm').submit();
+        return false;
+    }
 }
